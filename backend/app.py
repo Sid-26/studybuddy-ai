cf91e9f97ead95c89b7ce31ec321f86ecb5ac006
Sid-26 <siddhantdas262@gmail.com>
2025-11-30 23:55:33 -0500

    updated port for backend

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,7 @@ import validate
 import telemetry
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:8080"])
+CORS(app, origins=["http://localhost:9000"])
 
 @app.route('/')
 def test():
@@ -61,4 +61,4 @@ def generate_quiz():
     pass
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(debug=True, host="0.0.0.0", port=9000)
